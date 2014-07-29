41e330b24179b40387e67c51a98ad660f9b0364c
Emilien Kenler <ekenler@wizcorp.jp>
2014-07-29 18:55:58 +0900

    tab -> spaces

--- Editor/CustomPostprocessScript.cs
+++ Editor/CustomPostprocessScript.cs
@@ -10,25 +10,22 @@ using System.Diagnostics;
 public class CustomPostprocessScript : MonoBehaviour
 {
 
-	//IMPORTANT!!!
-	//100 is order , it means this one will execute after e.g 1 as default one is 1 
-	//it means our script will run after all other scripts got run
-	[PostProcessBuild(100)]
-	public static void OnPostprocessBuild(BuildTarget target, string pathToBuildProject)
-	{
-        
-		UnityEngine.Debug.Log("----Custome Script---Executing post process build phase."); 		
-		Process myCustomProcess = new Process();		
-		myCustomProcess.StartInfo.FileName = "python";
-		myCustomProcess.StartInfo.Arguments = string.Format("Assets/Editor/post_process.py \"{0}\"", pathToBuildProject);
-		myCustomProcess.StartInfo.UseShellExecute = false;
-		myCustomProcess.StartInfo.RedirectStandardOutput = false;
-		myCustomProcess.Start(); 
-		myCustomProcess.WaitForExit();
-		UnityEngine.Debug.Log("----Custome Script--- Finished executing post process build phase.");  
-		
-       
-	}
+    //IMPORTANT!!!
+    //100 is order , it means this one will execute after e.g 1 as default one is 1
+    //it means our script will run after all other scripts got run
+    [PostProcessBuild(100)]
+    public static void OnPostprocessBuild(BuildTarget target, string pathToBuildProject)
+    {
+        UnityEngine.Debug.Log("----Custome Script---Executing post process build phase.");
+        Process myCustomProcess = new Process();
+        myCustomProcess.StartInfo.FileName = "python";
+        myCustomProcess.StartInfo.Arguments = string.Format("Assets/Editor/post_process.py \"{0}\"", pathToBuildProject);
+        myCustomProcess.StartInfo.UseShellExecute = false;
+        myCustomProcess.StartInfo.RedirectStandardOutput = false;
+        myCustomProcess.Start();
+        myCustomProcess.WaitForExit();
+        UnityEngine.Debug.Log("----Custome Script--- Finished executing post process build phase.");
+    }
 }
 
 #endif
