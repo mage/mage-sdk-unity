d67b0869cec1c875b7a77215640b0af99b017dab
Daniel Dressler <danieru.dressler@gmail.com>
2015-08-07 16:41:30 +0900

    Add Event Emitter

---
+++ EventEmitter/EventEmitter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+public class EventEmitter<T> {
+	//
+	private Dictionary<string, object> eventTags = new Dictionary<string, object>();
+	private EventHandlerList eventsList = new EventHandlerList();
+
+	//
+	public void on(string eventTag, Action<object, T> handler)
+	{
+		if (!eventTags.ContainsKey(eventTag)) {
+			eventTags.Add(eventTag, new object());
+		}
+
+		eventsList.AddHandler(eventTags[eventTag], handler);
+	}
+
+	//
+	public void emit(string eventTag, object sender, T arguments)
+	{
+		if (!eventTags.ContainsKey(eventTag)) {
+			return;
+		}
+
+		Action<object, T> execEventList = (Action<object, T>)eventsList[eventTags[eventTag]];
+		execEventList(sender, arguments);
+	}
+	
+	public void emit(string eventTag, T arguments)
+	{
+		emit (eventTag, null, arguments);
+	}
+}
