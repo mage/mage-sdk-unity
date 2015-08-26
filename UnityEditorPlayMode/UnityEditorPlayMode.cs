88dcfddbb7ad02d942b28afff7df80758a3c56b2
Almir Kadric <github@almirkadric.com>
2015-08-26 12:36:56 +0900

    Fixed message stream to stop when the unity editor is stopped.

---
+++ UnityEditorPlayMode/UnityEditorPlayMode.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+
+using UnityEngine;
+using UnityEditor;
+
+
+public enum EditorPlayModeState
+{
+	Stopped,
+	Playing,
+	Paused
+}
+
+[InitializeOnLoad]
+public class UnityEditorPlayMode {
+	private static EditorPlayModeState currentState = EditorPlayModeState.Stopped;
+	public delegate void OnEditorModeChanged(EditorPlayModeState newState);
+	public static OnEditorModeChanged onEditorModeChanged;
+
+	static UnityEditorPlayMode()
+	{
+		EditorApplication.playmodeStateChanged += OnUnityPlayModeChanged;
+		if (EditorApplication.isPaused) {
+			currentState = EditorPlayModeState.Paused;
+		}
+	}
+
+	private static void OnUnityPlayModeChanged()
+	{
+		EditorPlayModeState newState = EditorPlayModeState.Stopped;
+		switch (currentState) {
+		case EditorPlayModeState.Stopped:
+			if (EditorApplication.isPlayingOrWillChangePlaymode) {
+				newState = EditorPlayModeState.Playing;
+			} else {
+				newState = EditorPlayModeState.Paused;
+			}
+			break;
+		case EditorPlayModeState.Playing:
+			if (EditorApplication.isPaused) {
+				newState = EditorPlayModeState.Paused;
+			} else if (EditorApplication.isPlaying) {
+				newState = EditorPlayModeState.Playing;
+			} else {
+				newState = EditorPlayModeState.Stopped;
+			}
+			break;
+		case EditorPlayModeState.Paused:
+			if (EditorApplication.isPlayingOrWillChangePlaymode && !EditorApplication.isPaused) {
+				newState = EditorPlayModeState.Playing;
+			} else if (EditorApplication.isPlayingOrWillChangePlaymode && EditorApplication.isPaused) {
+				newState = EditorPlayModeState.Paused;
+			}
+			break;
+		}
+
+		if (onEditorModeChanged != null) {
+			onEditorModeChanged.Invoke(newState);
+		}
+
+		currentState = newState;
+	}
+}
