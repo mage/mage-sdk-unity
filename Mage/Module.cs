cfc70bf9950c015f65bdc120cc05a041a5f489f4
Almir Kadric <github@almirkadric.com>
2017-10-10 00:22:03 +0900

    removed redundant code. also minor bugfixes

--- Mage/Module.cs
+++ Mage/Module.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 
 using Newtonsoft.Json.Linq;
@@ -17,127 +18,38 @@ namespace Wizcorp.MageSDK.MageClient
 			get { return Mage.Instance; }
 		}
 
+		//
 		protected Logger Logger
 		{
 			get { return Mage.Logger(GetType().Name); }
 		}
 
-
-		//
-		protected virtual List<string> StaticTopics
-		{
-			get { return null; }
-		}
-
-		public JToken StaticData;
-
-		public void SetupStaticData(Action<Exception> cb)
-		{
-			Logger.Info("Setting up static data");
-
-			if (StaticTopics == null)
-			{
-				cb(null);
-				return;
-			}
-
-			var queries = new JObject();
-			for (var i = 0; i < StaticTopics.Count; i++)
-			{
-				string topic = StaticTopics[i];
-
-				var query = new JObject {
-					{"topic", new JValue(topic)},
-					{"index", new JObject()}
-				};
-				queries.Add(topic, query);
-			}
-
-			StaticData = null;
-			Mage.Archivist.MGet(queries, null, (error, data) => {
-				if (error != null)
-				{
-					cb(error);
-					return;
-				}
-
-				StaticData = data;
-				cb(null);
-			});
-		}
-
-
 		//
 		protected virtual string CommandPrefix
 		{
-			get { return null; }
+			get {
+				return null;
+			}
 		}
 
-		protected virtual List<string> Commands
-		{
-			get { return null; }
-		}
-
-		private Dictionary<string, Action<JObject, Action<Exception, JToken>>> commandHandlerActions;
-		private Dictionary<string, Func<JObject, UserCommandStatus>> commandHandlerFuncs;
-
+		//
 		public void Command(string commandName, JObject arguments, Action<Exception, JToken> cb)
 		{
-			commandHandlerActions[commandName](arguments, cb);
+			Mage.CommandCenter.SendCommand(CommandPrefix + "." + commandName, arguments, cb);
 		}
 
+		//
 		public UserCommandStatus Command(string commandName, JObject arguments)
 		{
-			return commandHandlerFuncs[commandName](arguments);
-		}
+			var commandStatus = new UserCommandStatus();
 
-		private void RegisterCommand(string command)
-		{
-			commandHandlerActions.Add(command, (arguments, commandCb) => {
-				Mage.CommandCenter.SendCommand(CommandPrefix + "." + command, arguments, (error, result) => {
-					try
-					{
-						commandCb(error, result);
-					}
-					catch (Exception callbackError)
-					{
-						Logger.Data(callbackError).Critical("Uncaught exception:");
-					}
-				});
+			Mage.CommandCenter.SendCommand(CommandPrefix + "." + commandName, arguments, (error, result) => {
+				commandStatus.Error = error;
+				commandStatus.Result = result;
+				commandStatus.Done = true;
 			});
 
-			commandHandlerFuncs.Add(command, arguments => {
-				var commandStatus = new UserCommandStatus();
-
-				Mage.CommandCenter.SendCommand(CommandPrefix + "." + command, arguments, (error, result) => {
-					commandStatus.Error = error;
-					commandStatus.Result = result;
-					commandStatus.Done = true;
-				});
-
-				return commandStatus;
-			});
-		}
-
-		public void SetupUsercommands(Action<Exception> cb)
-		{
-			Logger.Info("Setting up usercommands");
-
-			commandHandlerActions = new Dictionary<string, Action<JObject, Action<Exception, JToken>>>();
-			commandHandlerFuncs = new Dictionary<string, Func<JObject, UserCommandStatus>>();
-
-			if (Commands == null)
-			{
-				cb(null);
-				return;
-			}
-
-			foreach (string command in Commands)
-			{
-				RegisterCommand(command);
-			}
-
-			cb(null);
+			return commandStatus;
 		}
 	}
 }
